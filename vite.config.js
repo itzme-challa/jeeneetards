a456f3dec938a54602b432820f5d50696ec4140a
itzme-challa <challawaheed4@gmail.com>
2025-01-20 21:56:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  build: {
+    outDir: '.svelte-kit/output/client',
+  },
+  server: {
+    fs: {
+      allow: ['src'],
+    },
+  },
+};
+
+export default config;
